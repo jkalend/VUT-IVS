7755ac02fa18de4f830d9969429629dab9e8b9db
kalen <jkalend@gmail.com>
2022-04-19 19:06:14 +0200

    fixed files according to PEP-8, added help
    inline comments offset by 4 spaces instead of 1
    added another space between function definitions
    added multiple missing spaces between operators
    removed additional spaces in default values of function arguments
    moved imports to the top of the source file
    added __name__ == '__main__' block to prevent gui to be launched indirectly
    
    additionally more comments were added
    python shebang added
    help button made functional

--- src/calc.py
+++ src/calc.py
@@ -4,16 +4,19 @@ def mult(a, b):
     Return their product"""
     return a * b
 
+
 def add(a, b):
     """Add two numbers.
     Return their sum."""
     return a + b
 
+
 def sub(a, b):
-    """Substract two numbers.
+    """Subtract two numbers.
     First number is minuend, second is subtrahend.
     Return their difference."""
     return a - b
+
 
 def div(a, b):
     """Divide two numbers.
@@ -24,6 +27,7 @@ def div(a, b):
     if b == 0:
         raise ValueError("MA ERROR: zero division")
     return a / b
+
 
 def factorial(a):
     """Take a single number and return its factorial.
@@ -36,6 +40,7 @@ def factorial(a):
         a -= 1
     return res
 
+
 def mod(a, b):
     """Divide two numbers.
     First number is dividend, second is divisor.
@@ -45,6 +50,7 @@ def mod(a, b):
     if int(a) != a or int(b) != b or b == 0:
         raise ValueError("MA error: invalid operands")
     return a % b
+
 
 def root(n, x):
     """Take two numbers, first one is the index, second is the radicand.
@@ -64,6 +70,7 @@ def root(n, x):
         else:
             return x ** (1.0 / n)
 
+
 def exp(x, n):
     """Take two numbers, first is base, second is exponent.
     Return x^n.
@@ -71,6 +78,7 @@ def exp(x, n):
     if x == 0 and n < 0:
         raise ValueError("MA ERROR: zero division")
     return x ** n
+
 
 def split_expression(expr):
     """Split the string representing the mathematical expression to a format required by eval_substr.
@@ -99,20 +107,21 @@ def split_expression(expr):
 
         elif x in operators:
             if x == '√' and (len(parsed) == 0 or parsed[-1] in operators and parsed[-1] != ')'):
-                parsed.append("2") # default is square root
+                parsed.append("2")    # default is square root
             parsed.append(x) 
 
         else:
             raise TypeError("MA error: unrecognized symbol")
     return parsed
 
-def eval_substr(parsed, in_expr = False):
+
+def eval_substr(parsed, in_expr=False):
     """Evaluate parsed representation of input string.
     Return the result of solving the mathematical expression.
     Raise an error if the expression cannot be solved."""
     
-    priority = [["("], ["!"],["^", "√"], ['-'], ["%", "/", "*"], ["-", "+"], [")"]] 
-    funcs = {"^":exp, "/": div, "*": mult, "-": sub, "+": add, "√" : root, "%": mod}
+    priority = [["("], ["!"], ["^", "√"], ['-'], ["%", "/", "*"], ["-", "+"], [")"]]
+    funcs = {"^": exp, "/": div, "*": mult, "-": sub, "+": add, "√": root, "%": mod}
 
     for operator in priority:
         i = 0
@@ -120,13 +129,13 @@ def eval_substr(parsed, in_expr = False):
             func = None
             x = parsed[i]
 
-            # start of nested expression, do a recursive call to solve whats inside
+            # start of nested expression, do a recursive call to solve what's inside
             if x == '(':
                 parsed[i:] = eval_substr(parsed[i + 1:], True)
 
             # solve only until nearest closing brace
             if x == ')':
-                if not in_expr: # no opening brace
+                if not in_expr:    # no opening brace
                     raise ValueError("MA error: missing brace")
                 # if we are at the end of solving the current sub-expression, remove the closing brace 
                 if operator == priority[-1]:
@@ -134,11 +143,11 @@ def eval_substr(parsed, in_expr = False):
                     in_expr = False
                 break
 
-            if x == '-' and len(operator) == 1 and x in operator: # sign invert
+            if x == '-' and len(operator) == 1 and x in operator:    # sign invert
                 if i - 1 < 0 or parsed[i - 1] in [k for j in priority for k in j] and str(parsed[i + 1])[-1].isdigit():
-                     parsed[i] = -float(parsed[i + 1])
-                     parsed[i + 1:] = parsed[i + 2:]
-                     i -= 1
+                    parsed[i] = -float(parsed[i + 1])
+                    parsed[i + 1:] = parsed[i + 2:]
+                    i -= 1
 
             elif x == '!' and x in operator: 
                 parsed[i - 1] = factorial(float(parsed[i - 1]))
@@ -148,7 +157,7 @@ def eval_substr(parsed, in_expr = False):
             elif x in operator and x in funcs.keys():
                 func = funcs[x]
 
-            if func != None:
+            if func is not None:
                 if parsed[i + 1] == '-':
                     parsed[i + 1] += str(parsed[i + 2])
                     parsed[i + 2:] = parsed[i + 3:]
@@ -157,10 +166,11 @@ def eval_substr(parsed, in_expr = False):
                 parsed[i:] = parsed[i + 2:]
                 i -= 1
             i += 1
-    if in_expr: #no closing brace
+    if in_expr:    # no closing brace
         raise ValueError("MA error: missing brace")
 
     return parsed
+
 
 def eval_str(expr):
     """Return the final result rounded to 10 decimal places."""
--- src/calcgui.py
+++ src/calcgui.py
@@ -1,25 +1,23 @@
-# usr/bin/python
-
-# priority = [["("], ["!"],["^", "√"], ['-'], ["%", "/", "*"], ["-", "+"], [")"]]
+#!usr/bin/python
 
 # import kivy module
 import kivy
+# import calculator math library
+import calc
+import webbrowser
 
 # base Class of your App inherits from the App class.
 # app:always refers to the instance of your application
 from kivy.app import App
-
-kivy.require('2.1.0')
 
 # for arranging widgets in a grid
 from kivy.uix.gridlayout import GridLayout
 
 # for the size of window
 from kivy.config import Config
-
 from kivy.lang import Builder
 
-import calc
+kivy.require('2.1.0')
 
 # Setting size to resizable
 Config.set('graphics', 'resizable', 1)
@@ -27,191 +25,212 @@ Config.set('graphics', 'resizable', 1)
 
 # Creating Layout class
 class CalcGridLayout(GridLayout):
+    # variable indicating whether to clear the display or not
     ERR = False
+
+    path = 'manual.pdf'
 
     # Function called when "=" is pressed
     def calculate(self, calculation):
         if calculation:
             try:
                 self.display.text = str(calc.eval_str(calculation))
-            except Exception:
+            except TypeError:
+                self.display.text = "INVALID INPUT"
+                self.ERR = True
+            except ValueError:
                 self.display.text = "MATH ERROR"
                 self.ERR = True
 
-    def help(self):
-        self.display.text = "HELP"
-        self.ERR = True
-
-    def delete(self):
-        if self.ERR:
-            self.display.text = ""
-            self.ERR = False
-        else:
-            self.display.text = self.display.text[:-1]
-
+    # Function called when "AC" button is pressed
     def clear(self):
         self.display.text = ""
         self.ERR = False
 
+    # Function called when "HELP" button is pressed
+    def help(self):
+        self.clear()
+        webbrowser.open(self.path)
+
+    # Function called when "DEL" button is pressed
+    def delete(self):
+        if self.ERR:
+            self.clear()
+        else:
+            self.display.text = self.display.text[:-1]
+
+    # Function handling the input of numbers and operators
     def enter(self, text):
         if self.ERR:
-            self.ERR = False
             self.display.text = text
+            self.ERR = False
         else:
             self.display.text += text
+
 
 # Creating App class
 class CalculatorApp(App):
 
     def build(self):
         file = Builder.load_string("""
-        # Custom button
+# Custom button
 <CustButton@Button>:
-	font_size: 32
+    font_size: 36
 
-# Define id so I can refer to the CalcGridLayout
-# class functions
-# Display points to the entry widget
+    # Defining id of the grid layout
+    # Display points to the entry widget
 <CalcGridLayout>:
-	id: calculator
-	display: entry
-	rows: 6
-	padding: 10
-	spacing: 10
+    id: calculator
+    display: entry
+    rows: 6
+    padding: 10
+    spacing: 10
 
 
 
-	# Where input is displayed
-	BoxLayout:
-		TextInput:
-			id: entry
-			font_size: 32
-			multiline: False
-			focus: True
-    
+    # Where input is displayed
     BoxLayout:
-		spacing: 10
-		CustButton:
-			text: "AC"
-			on_press: calculator.clear()
-			on_release: entry.focus = True
-		CustButton:
-			text: "DEL"
-			on_press: calculator.delete()
-			on_release: entry.focus = True
-		CustButton:
-			text: "("
-			on_press: calculator.enter("(")
-			on_release: entry.focus = True
-		CustButton:
-			text: ")"
-			on_press: calculator.enter(")")
-			on_release: entry.focus = True
-		CustButton:
-			text: "%"
-			on_press: calculator.enter("%")
-			on_release: entry.focus = True
-			
-
-	# When buttons are pressed update the entry
-	BoxLayout:
-		spacing: 10
-		CustButton:
-			text: "7"
-			on_press: calculator.enter("7")
-			on_release: entry.focus = True
-		CustButton:
-			text: "8"
-			on_press: calculator.enter("8")
-			on_release: entry.focus = True
-		CustButton:
-			text: "9"
-			on_press: calculator.enter("9")
-			on_release: entry.focus = True
-		CustButton:
-			text: "+"
-			on_press: calculator.enter("+")
-			on_release: entry.focus = True
+        TextInput:
+            id: entry
+            font_size: 32
+            multiline: False
+            focus: True
+            
+    # First row of buttons          
+    BoxLayout:
+        spacing: 10
         CustButton:
-			text: "-"
-			on_press: calculator.enter("-")
-			on_release: entry.focus = True
+            text: "AC"
+            on_press: calculator.clear()
+            on_release: entry.focus = True
+        CustButton:
+            text: "DEL"
+            on_press: calculator.delete()
+            on_release: entry.focus = True
+        CustButton:
+            text: "("
+            on_press: calculator.enter("(")
+            on_release: entry.focus = True
+        CustButton:
+            text: ")"
+            on_press: calculator.enter(")")
+            on_release: entry.focus = True
+        CustButton:
+            text: "%"
+            on_press: calculator.enter("%")
+            on_release: entry.focus = True
 
-	BoxLayout:
-		spacing: 10
-		CustButton:
-			text: "4"
-			on_press: calculator.enter("4")
-			on_release: entry.focus = True
-		CustButton:
-			text: "5"
-			on_press: calculator.enter("5")
-			on_release: entry.focus = True
-		CustButton:
-			text: "6"
+    # Second row of buttons
+    BoxLayout:
+        spacing: 10
+        CustButton:
+            text: "7"
+            on_press: calculator.enter("7")
+            on_release: entry.focus = True
+        CustButton:
+            text: "8"
+            on_press: calculator.enter("8")
+            on_release: entry.focus = True
+        CustButton:
+            text: "9"
+            on_press: calculator.enter("9")
+            on_release: entry.focus = True
+        CustButton:
+            text: "+"
+            on_press: calculator.enter("+")
+            on_release: entry.focus = True
+        CustButton:
+            text: "-"
+            on_press: calculator.enter("-")
+            on_release: entry.focus = True
+
+    # Third row of buttons
+    BoxLayout:
+        spacing: 10
+        CustButton:
+            text: "4"
+            on_press: calculator.enter("4")
+            on_release: entry.focus = True
+        CustButton:
+            text: "5"
+            on_press: calculator.enter("5")
+            on_release: entry.focus = True
+        CustButton:
+            text: "6"
             on_press: calculator.enter("6")
-			on_release: entry.focus = True
-		CustButton:
-			text: "*"
-			on_press: calculator.enter("*")
-			on_release: entry.focus = True
+            on_release: entry.focus = True
         CustButton:
-			text: "/"
-			on_press: calculator.enter("/")
-			on_release: entry.focus = True
-
-	BoxLayout:
-		spacing: 10
-		CustButton:
-			text: "1"
-			on_press: calculator.enter("1")
-			on_release: entry.focus = True
-		CustButton:
-			text: "2"
-			on_press: calculator.enter("2")
-			on_release: entry.focus = True
-		CustButton:
-			text: "3"
-			on_press: calculator.enter("3")
-			on_release: entry.focus = True
-		CustButton:
-			text: "^"
-			on_press: calculator.enter("^")
-			on_release: entry.focus = True
+            text: "*"
+            on_press: calculator.enter("*")
+            on_release: entry.focus = True
         CustButton:
-			text: "√"
-			on_press: calculator.enter("√")
-			on_release: entry.focus = True
+            text: "/"
+            on_press: calculator.enter("/")
+            on_release: entry.focus = True
 
-	# When equals is pressed pass text in the entry
-	# to the calculate function
-	BoxLayout:
-		spacing: 10
-		CustButton:
-			text: "!"
-			on_press: calculator.enter("!")
-			on_release: entry.focus = True
-		CustButton:
-			text: "0"
-			on_press: calculator.enter("0")
-			on_release: entry.focus = True
-		CustButton:
-			text: "="
-			on_press: calculator.calculate(entry.text)
-			on_release: entry.focus = True
-		CustButton:
-			text: "."
-			on_press: calculator.enter(".")
-			on_release: entry.focus = True
-		CustButton:
-			text: "HELP"
-			on_press: calculator.help()
-			on_release: entry.focus = True
+    # Fourth row of buttons
+    BoxLayout:
+        spacing: 10
+        CustButton:
+            text: "1"
+            on_press: calculator.enter("1")
+            on_release: entry.focus = True
+        CustButton:
+            text: "2"
+            on_press: calculator.enter("2")
+            on_release: entry.focus = True
+        CustButton:
+            text: "3"
+            on_press: calculator.enter("3")
+            on_release: entry.focus = True
+        CustButton:
+            text: "^"
+            on_press: calculator.enter("^")
+            on_release: entry.focus = True
+        CustButton:
+            text: "√"
+            on_press: calculator.enter("√")
+            on_release: entry.focus = True
+
+    # Fifth row of buttons
+    # When equals is pressed pass text in the entry
+    # to the calculate function
+    BoxLayout:
+        spacing: 10
+        CustButton:
+            text: "!"
+            on_press: calculator.enter("!")
+            on_release: entry.focus = True
+        CustButton:
+            text: "0"
+            on_press: calculator.enter("0")
+            on_release: entry.focus = True
+        CustButton:
+            text: "="
+            on_press: calculator.calculate(entry.text)
+            on_release: entry.focus = True
+        CustButton:
+            text: "."
+            on_press: calculator.enter(".")
+            on_release: entry.focus = True
+        CustButton:
+            text: "HELP"
+            on_press: calculator.help()
+            on_release: entry.focus = True
 
 """)
         return CalcGridLayout()
 
+# Create the App and load the kv file
+# The kv file is a python file that contains the
+# definition of the widgets and the layout of the app
+# The kv file is loaded by the Builder.
+# The Builder loads the kv file and creates the widgets
+# and the layout of the app.
 
-# creating object and running it
-calcApp = CalculatorApp()
-calcApp.run()
+
+# GUI is run only if this file is run directly
+if __name__ == '__main__':
+    # creating object and running it
+    calcApp = CalculatorApp()
+    calcApp.run()
